f506a47f575ac13981d2318d2fd1aeac0ae6d764
alexpilot <a41751@gmail.com>
2020-04-04 11:03:31 +0300

    add model user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+    foto = models.ImageField('Фото',upload_to='users/foto',default='',blank=True)
+    nik = models.CharField(max_length=15,verbose_name='Ник')
+    firstname = models.CharField(max_length=15,verbose_name='Имя')
+    lastname = models.CharField(max_length=15,verbose_name='Фамилия')
+    pfone = models.CharField(max_length=12,blank=True,verbose_name='Телефон')
+    dborn = models.DateField(auto_now=False,auto_now_add=False,blank=True,verbose_name='Дата рождения')
+    email = models.EmailField(max_length=50,blank=True,verbose_name='Email')
+    rate = models.IntegerField(default=0,verbose_name='Рейтинг')
+    class meta:
+        verbose_name='Член клуба'
+        verbose_name_plural='Члены клуба'
+        ordering=['lastname']
+    def __str__(self):
+        return self.lastname
+        
