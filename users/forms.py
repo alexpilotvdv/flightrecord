0f9fec815992ef598a557949b08340da04191896
alexpilot <a41751@gmail.com>
2020-04-09 16:21:50 +0300

    maked register whith check field

---
+++ users/forms.py
@@ -0,0 +1,31 @@
+from django.forms import ModelForm
+from django import forms
+from django.contrib import auth
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class UserCreateForm(UserCreationForm):
+    email=forms.EmailField(required=True)
+    prov=forms.CharField(max_length="10")
+    #попробовал добавить проверочное поле в форму
+    prov.label="Проверочный код"
+
+    class Meta:
+        model=User
+        fields=("username","email","password1","password2","prov")
+
+    def save(self,commit=True):
+        user=super(UserCreateForm,self).save(commit=False)
+        user.email=self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
+    #проверка поля
+    def clean(self):
+        cleaned_data=super(UserCreateForm,self).clean()
+        prov_val=cleaned_data.get("prov")
+        if '120' not in prov_val:
+            msg="Не правильный проверочный код!"
+            self._errors["prov"]=self.error_class([msg])
+            #del cleaned_data["prov"]
+        return cleaned_data
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from users.models import user,flightday
+from django.views.generic import TemplateView
 #для регистрации пользователя
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
@@ -8,6 +9,7 @@ from django.contrib.auth import login
 from django.contrib.auth import logout
 from django.views.generic.edit import FormView
 from django.views.generic.base import View
+from .forms import UserCreateForm
 
 # Create your views here.
 def users(request):
@@ -21,12 +23,30 @@ def main_page(request):
     return render(request,'index.html')
 #регистрация нового пользователя
 class RegisterFormView(FormView):
-    form_class=UserCreationForm
+    form_class=UserCreateForm
     success_url="/login/"
     template_name="users/register.html"
     def form_valid(self,form):
-        form.save()
-        return super(RegisterFormView,self).form_valid(form)
+        #вариант проверки проверочного поля
+        #print(form.cleaned_data['prov'])
+    #    if (form.cleaned_data['prov'] == '120'):
+            form.save()
+            return super(RegisterFormView,self).form_valid(form)
+        #else:
+            #return super(RegisterFormView,self).form_invalid(form)
+
+    def form_invalid(self,form):
+        return super(RegisterFormView,self).form_invalid(form)
+
+
+class MainView(TemplateView):
+    template_name="index.html"
+    def get(self,request):
+        if request.user.is_authenticated:
+            return render(request,self.template_name,{})
+        else:
+            return render(request,self.template_name,{})
+
 
 # аутентификация
 class LoginFormView(FormView):
